f13113447e6cdf1ef88237d815293163fe6dc980
stonefullstm <stonefull.stm@gmail.com>
2023-05-06 17:56:51 -0300

    feat: Requisito 2 - Implementação da função text_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise TypeError
+        file = open(path_file, mode="r")
+        return [line.rstrip("\n") for line in file]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    except TypeError:
+        sys.stderr.write("Formato inválido")
