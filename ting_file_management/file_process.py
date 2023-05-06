bd1f8549ee5908e4f2fec5845ba3ae7b7574122d
stonefullstm <stonefull.stm@gmail.com>
2023-05-06 19:30:50 -0300

    feat: Requisito 3 - Implementação da função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines = txt_importer(path_file)
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines
+    }
+    index = 0
+    while (index < len(instance)
+           and instance.search(index)["nome_do_arquivo"] != path_file):
+        index += 1
+    if index >= len(instance):
+        instance.enqueue(file_dict)
+        sys.stdout.write(str(file_dict))
 
 
 def remove(instance):
