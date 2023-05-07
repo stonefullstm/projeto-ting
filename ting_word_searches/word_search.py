c99efe18549edf6cb93e0af8dabf78213d1b023d
stonefullstm <stonefull.stm@gmail.com>
2023-05-07 17:19:09 -0300

    Requisito 7 - Implementação da função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_in_files = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        lines = []
+        line = 0
+        for file_line in file["linhas_do_arquivo"]:
+            line += 1
+            if word.lower() in file_line.lower():
+                lines.append({"linha": line})
+        if lines:
+            word_in_files.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines
+            })
+    return word_in_files
 
 
 def search_by_word(word, instance):
