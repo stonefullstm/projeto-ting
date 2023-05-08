ecc290c617d6d953417858da87064d522ed1fc5d
stonefullstm <stonefull.stm@gmail.com>
2023-05-08 16:56:10 -0300

    test: Requisito 6 - Teste da classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
+
+
+file1 = {"qtd_linhas": 10}
+file2 = {"qtd_linhas": 12}
+file3 = {"qtd_linhas": 8}
+file4 = {"qtd_linhas": 3}
+file5 = {"qtd_linhas": 4}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priorityQueue = PriorityQueue()
+    priorityQueue.enqueue(file1)
+    priorityQueue.enqueue(file2)
+    priorityQueue.enqueue(file3)
+
+    assert priorityQueue.dequeue() == file1
+    priorityQueue.enqueue(file4)
+    priorityQueue.enqueue(file5)
+    assert priorityQueue.dequeue() == file4
+    assert priorityQueue.search(len(priorityQueue) - 1) == file3
+    with pytest.raises(IndexError):
+        priorityQueue.search(10)
